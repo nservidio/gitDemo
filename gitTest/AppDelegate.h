80955c1005e3d87ba5e1f16b1cafea8c3a7983e6
Nick.Servidio <nickservidio@BOS-MAC-NSERVID.local>
2015-01-26 12:20:09 -0500

    Initial Commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by Nick.Servidio on 1/26/15.
+//  Copyright (c) 2015 Nick.Servidio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by Nick.Servidio on 1/26/15.
+//  Copyright (c) 2015 Nick.Servidio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
