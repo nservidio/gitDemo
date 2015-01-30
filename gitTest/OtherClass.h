9fa19a408027cddb3043185d9da42a554e7b47f3
Nick Servidio <nick@intrepid.io>
2015-01-30 15:21:27 -0500

    Added OTHER class

---
+++ gitTest/OtherClass.h
@@ -0,0 +1,13 @@
+//
+//  OtherClass.h
+//  gitTest
+//
+//  Created by Nick.Servidio on 1/30/15.
+//  Copyright (c) 2015 Nick.Servidio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OtherClass : NSObject
+
+@end
---
+++ gitTest/OtherClass.m
@@ -0,0 +1,13 @@
+//
+//  OtherClass.m
+//  gitTest
+//
+//  Created by Nick.Servidio on 1/30/15.
+//  Copyright (c) 2015 Nick.Servidio. All rights reserved.
+//
+
+#import "OtherClass.h"
+
+@implementation OtherClass
+
+@end
