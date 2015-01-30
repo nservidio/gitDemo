fc668877334c0d89fecc2768fe7fb6b18edb06f3
Nick Servidio <nick@intrepid.io>
2015-01-30 15:20:24 -0500

    Added MyClass

---
+++ gitTest/MyClass.m
@@ -0,0 +1,13 @@
+//
+//  MyClass.m
+//  gitTest
+//
+//  Created by Nick.Servidio on 1/30/15.
+//  Copyright (c) 2015 Nick.Servidio. All rights reserved.
+//
+
+#import "MyClass.h"
+
+@implementation MyClass
+
+@end
